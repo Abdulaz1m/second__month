73ea347077b7280aef9570d0e5795583af1615f0
Abdulaz1m <azim.kojoev1987@gmail.com>
2022-03-10 21:33:30 +0300

    dz

---
+++ lesson 2/dz/fizz_buzz.js
@@ -0,0 +1,14 @@
+for (let i = 1; i < 101; i++){
+    if (i%3 && i%5 !== 0){
+        console.log(i)
+    }
+    if (i%3 === 0 && i%5 !== 0){
+        console.log(i + ":" + "Fizz")
+    }
+    if (i%5 === 0 && i%3 !== 0){
+        console.log(i + ':' + "Buzz")
+    }
+    if (i%3 ===0 && i%5 === 0){
+        console.log(i + ':' + "FizzBuzz")
+    }
+}
---
+++ lesson 2/dz/triangle.js
@@ -0,0 +1,7 @@
+const triangle = [
+    "*", "**", "***", "****", "*****", "******", "*******"
+]
+
+for (let i = 0; i < triangle.length; i++){
+    console.log(triangle[i])
+}
