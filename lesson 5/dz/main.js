e7142cbd443272fb04d42c3087798f80e0e81c37
Abdulaz1m <azim.kojoev1987@gmail.com>
2022-03-21 15:44:07 +0300

    dz

--- lesson 5/dz/main.js
+++ lesson 5/dz/main.js
@@ -1,22 +1,39 @@
-document.getElementById("red").onclick = function() {
+document.getElementById("red").onclick = function redClick() {
     document.getElementById("red").setAttribute("class", "red_click")
     document.getElementById("yellow").setAttribute("class", "yellow")
     document.getElementById("green").setAttribute("class", "green")
     alert("Стоп! Светофор горит красным!")
 }
-document.getElementById("yellow").onclick = function() {
+document.getElementById("yellow").onclick = function yellowCLick() {
     document.getElementById("red").setAttribute("class", "red")
     document.getElementById("yellow").setAttribute("class", "yellow_click")
     document.getElementById("green").setAttribute("class", "green")
     alert("Подождите, еще чуть-чуть...")
 }
-document.getElementById("green").onclick = function() {
+document.getElementById("green").onclick = function greenCLick() {
     document.getElementById("red").setAttribute("class", "red")
     document.getElementById("yellow").setAttribute("class", "yellow")
     document.getElementById("green").setAttribute("class", "green_click")
     alert("Можете идти! ")
 }
 
+const svetofor = prompt("Какой свет выберете? Красный - Желтый - Зеленый")
+switch (svetofor) {
+    case "Красный":
+        document.getElementById("red").setAttribute("class", "red_click");
+        alert("Стоп! Светофор горит красным!");
+        break;
+    case "Желтый":
+        document.getElementById("yellow").setAttribute("class", "yellow_click");
+        alert("Подождите, еще чуть-чуть...");
+        break;
+    case "Зеленый":
+        document.getElementById("green").setAttribute("class", "green_click");
+        alert("Можете идти! ");
+        break;
+    default:
+        alert("Куда едем?!")
+}
 
 
 
